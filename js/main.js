46efb5defd8d7758dc44ca12c3673ec0a9137f46
El-Leto <elleto.life@gmail.com>
2021-02-01 17:33:28 +0300

    Исправление замечаний по первым функциям

--- js/main.js
+++ js/main.js
@@ -1,30 +1,21 @@
 const getRandomNumber = function(min, max) {
+  if (min < 0 || max <= min) {
+    console.error('Неверные значения')
+    return null;
+  }
+
   return Math.random() * (max - min + 1) + min;
 }
 
-getRandomNumber(1, 20);
-//console.log(getRandomNumber(1, 20));
+//Источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
 
 const getRandomInteger = function(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-
-  if (min < 0 || max < 0 ||  max <= min) {
-    return undefined;
-  }
-
   return Math.floor(getRandomNumber(min, max));
 }
 
-getRandomInteger(0, 10);
-//console.log(getRandomInteger(0, 10));
-
 const getRandomFixed = function (min, max, fixedNumber) {
-  const rand = getRandomNumber(min, max);
+  const rand = +(getRandomNumber(min, max));
 
   return rand.toFixed(fixedNumber);
 }
-
-getRandomFixed(1, 20, 3);
-//console.log(getRandom(1, 20, 3));
