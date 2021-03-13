933f56daccee0e10c6cb8325d75c6ee9fb71625c
Keks <keks@htmlacademy.ru>
2021-03-13 14:47:25 +0300

    :heavy_check_mark: Сборка #7

--- 7/js/main.js
+++ 7/js/main.js
@@ -1,19 +1,5 @@
-import { getSimilarObjects } from './data.js';
-import { renderCard } from './card.js';
-import './user-form.js';
-import { disabledForm, disabledFilter } from './map-disable.js';
+import { deactivate } from './map-disable.js';
 import { initMap } from './map.js';
 
-const similarObjects = getSimilarObjects();
-const [similarObjectFirstElement] = similarObjects;
-const list = document.querySelector('.map__canvas');
-
-const createCard = () => {
-  list.appendChild(renderCard(similarObjectFirstElement));
-};
-
-createCard; //Для проверки на гите
-
-disabledForm();
-disabledFilter();
+deactivate();
 initMap();
